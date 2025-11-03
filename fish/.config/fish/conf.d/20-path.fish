4078cdf450d33c7d835ad9c3759019de58b61d2e
yuuki-fuji <yuukikun666@gmail.com>
2025-11-03 17:24:30 +0900

    update fish go path

--- fish/.config/fish/conf.d/20-path.fish
+++ fish/.config/fish/conf.d/20-path.fish
@@ -1,3 +1,12 @@
 # === PATH Settings ===
 set -Ux PATH $HOME/go/bin $PATH
 set -Ux PATH /usr/local/bin $PATH
+
+# Go
+if not contains /usr/local/go/bin $PATH
+    set -gx PATH /usr/local/go/bin $PATH
+end
+
+if not contains $HOME/go/bin $PATH
+    set -gx PATH $HOME/go/bin $PATH
+end
